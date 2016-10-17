32a3f8cc84ac5c481d4fd04cf3da6eadcdd0a594
Zihao <zihaopzhu@gmail.com>
2016-10-17 00:45:33 -0400

    initial commit

---
+++ scrapper.py
@@ -0,0 +1,14 @@
+import re
+import praw
+user_agent = ("FRESH rookie 1.0")
+r= praw.Reddit(user_agent = user_agent)
+subreddit = r.get_subreddit("hiphopheads")
+def query():
+	fresh_tracks = []
+	for submission in subreddit.get_hot(limit=20):
+		if re.search('\[FRESH]', submission.title):
+			fresh_tracks=fresh_tracks + [submission.title]
+	return fresh_tracks
+l=query()
+for song in l:
+		print song
---
+++ tweeter.py
@@ -0,0 +1,15 @@
+import os
+import twitter
+import scrapper
+#Get account information from console.
+token=os.environ['TWITTER_ACCESS_TOKEN_KEY']
+token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+key=os.environ['TWITTER_CONSUMER_KEY']
+key_secret=os.environ['TWITTER_CONSUMER_SECRET']
+#log in to twitter
+auth=twitter.OAuth(token, token_secret, key, key_secret)
+client = twitter.Twitter(auth=auth)
+fresh_tracks = scrapper.query()
+for track in fresh_tracks:
+	client.statuses.update(status=track)
+
