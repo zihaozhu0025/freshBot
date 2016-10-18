1d4cc86e49a94e439a1315e573389db6b247c66d
Zihao <zihaopzhu@gmail.com>
2016-10-17 21:37:34 -0400

    displays url and title is possible

--- tweeter.py
+++ tweeter.py
@@ -12,7 +12,13 @@ auth=twitter.OAuth(token, token_secret, key, key_secret)
 client = twitter.Twitter(auth=auth)
 while True:
 	fresh_tracks = scrapper.query()
-	for track in fresh_tracks:
-		client.statuses.update(status=track)
+	for track in fresh_tracks:	
+		url = track[1]
+		title = track[0]
+		if len(url) + len(track[0]) < 140:
+				post = title + " " +  url
+		else:
+			post = url
+		client.statuses.update(status=post)
 		print "querying!!!"
-	time.sleep(3600)
+	time.sleep(900)
